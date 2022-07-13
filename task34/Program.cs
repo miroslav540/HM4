450a9e2dfc77a0b4f785a1d698e965f9288cddb8
Miroslav <mir19972905@mai.ru>
2022-07-13 22:11:28 +0300

    task 34

---
+++ task34/Program.cs
@@ -0,0 +1,25 @@
+﻿static void FindEvens(string[] args)   
+{
+    int evens;
+    Console.WriteLine("Введите размер массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        int A = (new Random()).Next(100,999); 
+        result[i] = A;
+        Console.Write(result[i] + " ");
+    }
+    evens = 0;
+    for(int i = 0; i < size; i++)
+    {
+        if(result[i] % 2 == 0)
+        {
+            evens++;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Количество четных: {0}\n", evens);
+}
+
+FindEvens(args);
