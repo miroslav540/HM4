01a1ed2e38d6cb4fcc68661d546ec6d6e9819329
Салопаев Алексей <hegin4@gmail.com>
2022-07-14 21:13:46 +1000

    Предожение по 38 задаче

--- task38/Program.cs
+++ task38/Program.cs
@@ -1,30 +1,28 @@
 ﻿static void DifOfMinAndMax(string[] args)
 {
-    double dif = 0;
+    double dif;
     double sum = 0;
-    int max;
-    int min;
+    int max = 0;
+    int min = 100;
     Console.WriteLine("Enter how many numbers u want");
     int size = Convert.ToInt32(Console.ReadLine());
     int[] arr = new int[size];
-    max = arr[0];
-    min = arr[0];
     for (int i = 0; i < size; i++)
     {
         arr[i] = (new Random()).Next(1,100);
-        if(arr[i] < min) // я не знаю почему, но так работает, по другому не работает, может за решение и ошибка, но ответ всегда верный)
+        if (arr[i] >= max)
         {
-            max=arr[i];
+            max = arr[i];
         }
-        if (arr[i] > max)// если меня переменные как надо, минимум всегда равен = 0, я не понял как ее решить по другому
+        if (arr[i] <= min)
         {
-            min=arr[i];
+            min = arr[i];
         }
         sum += arr[i];
-         Console.WriteLine(arr[i] + " ");
+        Console.WriteLine(arr[i] + " ");
     } 
-    dif = min - max;
-    Console.WriteLine("sum = {0}, difference = {1}, max = {2}, min = {3}", sum, dif, min, max);// изобрел свой первый кастыль)
+    dif = max - min;
+    Console.WriteLine("sum = {0}, difference = {1}, max = {2}, min = {3}", sum, dif, max, min);// изобрел свой первый кастыль)
 }
 
 DifOfMinAndMax(args);
